065d3114c197f0c389d3c4370e0763db4141d7f2
AIFSH <149982694+AIFSH@users.noreply.github.com>
2024-04-30 16:27:59 +0800

    Update uploadVideo.js

--- web/js/uploadVideo.js
+++ web/js/uploadVideo.js
@@ -193,7 +193,7 @@ function videoUpload(node, inputName, inputData, app) {
 ComfyWidgets.VIDEOPLOAD = videoUpload;
 
 app.registerExtension({
-	name: "IP_LAP.UploadVideo",
+	name: "MuseTalk.UploadVideo",
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
 		if (nodeData?.name == "LoadVideo") {
 			nodeData.input.required.upload = ["VIDEOPLOAD"];
